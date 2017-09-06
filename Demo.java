b7c9baeafbb6f086b5caa5d04dc9e9c3ec892c3b
Daniel Wang <danielwang1228@sina.com>
2017-09-06 23:44:39 +0800

    Demo.java chencked in brh

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("This is a demo")
-        System.out.println("New branch changed")
+        System.out.println("This is a demo");
+        System.out.println("New branch changed");
+        System.out.println("brh commit");
     }
 }
